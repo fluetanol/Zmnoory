09cc8de59f7f9f9ce1329599d1dbab4539da81c8
regular <regular.jk.kim@gmail.com>
2025-07-25 14:02:07 +0900

    S13P11A302-129-be-member
    modified: 추천인 기능 추가, 포인트 관리 로직 생성 및 예외 처리 적용

--- backend/ZMNNOORY/src/main/java/com/gradation/zmnnoory/domain/member/dto/response/MemberResponse.java
+++ backend/ZMNNOORY/src/main/java/com/gradation/zmnnoory/domain/member/dto/response/MemberResponse.java
@@ -14,13 +14,17 @@ public class MemberResponse {
     private final String nickname;
     private final LocalDate birthday;
     private final Gender gender;
+    private final String recommenderEmail;
+    private final Long point;
 
     @Builder
-    private MemberResponse(String email, String nickname, LocalDate birthday, Gender gender) {
+    private MemberResponse(String email, String nickname, LocalDate birthday, Gender gender, String recommenderEmail, Long point) {
         this.email = email;
         this.nickname = nickname;
         this.birthday = birthday;
         this.gender = gender;
+        this.recommenderEmail = recommenderEmail;
+        this.point = point;
     }
 
     public static MemberResponse of(Member member) {
@@ -29,6 +33,8 @@ public class MemberResponse {
                 .nickname(member.getNickname())
                 .birthday(member.getBirthday())
                 .gender(member.getGender())
+                .recommenderEmail(member.getRecommender() != null ? member.getRecommender().getEmail() : "추천인 없음")
+                .point(member.getPoint())
                 .build();
     }
 }
