4481f8708859ec5f81f7d479e2b2a5ab6c539dff
박시우 <pswudo@gmail.com>
2025-08-04 09:45:40 +0900

    S13P11A302-172-be-api
    닉네임 중복 확인, 이메일 중복 확인

--- backend/ZMNNOORY/src/main/java/com/gradation/zmnnoory/domain/member/repository/MemberRepository.java
+++ backend/ZMNNOORY/src/main/java/com/gradation/zmnnoory/domain/member/repository/MemberRepository.java
@@ -12,4 +12,5 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
     Optional<Member> findByEmail(String email);
 
     boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
 }
