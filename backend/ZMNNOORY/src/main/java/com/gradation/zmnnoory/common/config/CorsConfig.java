d2035217bac7da40173c3bf4e48edffcaab90ff4
김광현 <kyn06114@gmail.com>
2025-08-12 11:09:43 +0900

    S13P11A302-279 fix: cors 수정

--- backend/ZMNNOORY/src/main/java/com/gradation/zmnnoory/common/config/CorsConfig.java
+++ backend/ZMNNOORY/src/main/java/com/gradation/zmnnoory/common/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:5175",
+                        "http://localhost:5173",
                         "https://zmnoory.vercel.app"
                 )
                 .allowedMethods("*")
